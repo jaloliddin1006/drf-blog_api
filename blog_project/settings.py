51d2b1a1a59af37b01a809814ae815d3bcc3f171
Jaloliddin <jmamatmusayev@gmail.com>
2023-04-29 00:14:23 +0500

    authentication

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -37,13 +37,28 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'posts',
+    'django.contrib.sites',
+
     'rest_framework',
+    'rest_framework.authtoken',
+    'dj_rest_auth', 
+    'dj_rest_auth.registration', 
+    'allauth',
+    'allauth.account',
+
+
+    'posts',
 ]
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+SITE_ID = 1
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES':[
-        'rest_framework.permissions.IsAuthenticated'
+        'rest_framework.permissions.AllowAny'
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ]
 }
 
