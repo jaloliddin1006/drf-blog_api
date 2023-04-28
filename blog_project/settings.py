f3e03a14e463bdcb4731525596d1364c3cf7d4af
Jaloliddin <jmamatmusayev@gmail.com>
2023-04-29 03:07:34 +0500

    sxema va documentatsiya

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'dj_rest_auth.registration', 
     'allauth',
     'allauth.account',
+    'drf_yasg',
 
 
     'posts',
--- blog_project/urls.py
+++ blog_project/urls.py
@@ -16,7 +16,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+from drf_yasg import openapi
 
+schema_view = get_schema_view(
+    openapi.Info(
+    title="Blog API",
+    description="Django rest framevorkida tayyorlangan blog apisi",
+    default_version="v1",
+    terms_of_service="https://www.google.com/policies/terms/",
+    contact=openapi.Contact(email="j.mamatmusayev@tuit.uz"),
+    license=openapi.License(name="Blog project uchun litsensiya"),
+
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('posts.urls')),
@@ -24,4 +40,10 @@ urlpatterns = [
     path('api/v1/dj-rest-auth/', include('dj_rest_auth.urls')),
     path('api/v1/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('api/allauth/', include('allauth.urls')),
+    path('swagger/', schema_view.with_ui(
+        'swagger', cache_timeout=0
+        ), name="schema-swagger-ui"),
+    path('redoc/', schema_view.with_ui(
+        'redoc', cache_timeout=0
+        ), name="schema-redoc")
 ]
